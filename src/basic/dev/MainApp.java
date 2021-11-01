1fb3d407ecd4ae7a5ea3ed17eada84178694f469
unknown <My Tien>
2021-11-01 15:41:07 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,49 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {	
+		// TODO Auto-generated method stub
+		String firstName = " Tran Van Thanh";
+		String lastName = " Dat";
+		int i = 2160717;
+		int x = 191923723;
+		String a = "21CDTH41";
+		String b = "Cao Dang Cong Nghiep";
+		float l = 9.5f;
+		String z = "Quy";
+        String q = "15 Nguyen Hue";
+		String s = " 01658843878"; 
+		float u = 7.8f;
+		String fullName = "Cup";
+		String k = "Honda";
+		float g = 500000f ;
+		String j = "Do";
+		
+		
+		System.out.println("Họ và tên:" + firstName + lastName);
+		
+		System.out.println("Mã sinh viên:" +  i );
+		
+		System.out.println("chứng minh nhân dân:" + x);
+		
+		System.out.println("Ten lop : " + a);
+		
+        System.out.println("Ten truong :" + b);		
+		
+		System.out.println("Diem trung binh :" + l);
+		
+		System.out.println("THONG TIN CONG DAN " + " ten: " + z);
+        
+		System.out.println("Dia chi :" + q);
+		
+		System.out.println("So dien thoai : " + s);
+		System.out.println("He so luong : " + u);
+		System.out.println("THONG TIN MOT CHIEC XE CAR");
+		System.out.println("Ten : " + fullName);
+		System.out.println("hang : " + k);
+		System.out.println("gia : " + g);
+		System.out.println( "Mau son : " + j);
+	}
+
+}
